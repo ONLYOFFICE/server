32e1435321414cefff7413b49e4c834daf257efd
PauI Ostrovckij <pavel.ostrovskij@onlyoffice.com>
2025-08-04 11:19:07 +0300

    feat: admin panel react

---
+++ DocService/sources/routes/config/config.service.js
@@ -0,0 +1,136 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2024
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
+'use strict';
+const Joi = require('joi');
+const validation = require('./validation');
+const tenantManager = require('../../../../Common/sources/tenantManager');
+const utils = require('../../../../Common/sources/utils');
+
+const tenantReadableFields = [
+  'services.CoAuthoring.expire',
+  'FileConverter.converter.maxDownloadBytes',
+]
+
+const adminReadableFields = [
+  'services.CoAuthoring.expire',
+  'FileConverter.converter.maxDownloadBytes',
+];
+
+function createSchema(isAdmin) {
+  const baseSchema = {
+    services: Joi.object({
+        CoAuthoring: Joi.object({
+            expire: Joi.object({
+               ...(isAdmin && {
+                filesCron: validation.cronSchema,
+               }),
+               ...(isAdmin && {
+                documentsCron: validation.cronSchema,
+               }),
+               ...(isAdmin && {
+                files: Joi.number().min(0),
+               }),
+               ...(isAdmin && {
+                filesremovedatonce: Joi.number().min(0),
+               }),
+               ...(isAdmin && {
+                filesremovedatonce: Joi.number().min(0),
+               }),
+               
+            }).unknown(false),
+            autoAssembly: Joi.object({
+                step: Joi.any().valid('1m', '5m', '10m', '15m', '30m'),
+            }).unknown(false),
+        }).unknown(false)
+    }).unknown(false),
+    FileConverter: Joi.object({
+        converter: Joi.object({
+            maxDownloadBytes: Joi.number().min(0).max(104857600),
+        }).unknown(false)
+    }).unknown(false)
+  };
+  
+  return baseSchema;
+}
+
+function getReadableFields(ctx) {
+  return tenantManager.isMultitenantMode(ctx) && !tenantManager.isDefaultTenant(ctx)
+    ? tenantReadableFields
+    : adminReadableFields;
+}
+
+function getValidationSchema(ctx) {
+  const isAdmin = !tenantManager.isMultitenantMode(ctx) || tenantManager.isDefaultTenant(ctx);
+  return createSchema(isAdmin);
+}
+
+function validate(updateData, ctx) {
+    const schema = getValidationSchema(ctx);
+    return Joi.object(schema).validate(updateData, { abortEarly: false });
+}
+
+function getFilteredConfig(ctx) {
+  const cfg = ctx.getFullCfg();
+  const readableFields = getReadableFields(ctx);
+  const filteredConfig = {};
+  
+  readableFields.forEach(field => {
+    const value = utils.getImpl(cfg, field);
+    if (value !== undefined) {
+      set(filteredConfig, field, value);
+    }
+  });
+  
+  return filteredConfig;
+}
+
+function set(obj, path, value) {
+  const keys = path.split('.');
+  let current = obj;
+  
+  for (let i = 0; i < keys.length - 1; i++) {
+    const key = keys[i];
+    if (!(key in current) || typeof current[key] !== 'object' || current[key] === null) {
+      current[key] = {};
+    }
+    current = current[key];
+  }
+  
+  current[keys[keys.length - 1]] = value;
+  return obj;
+}
+
+module.exports = {
+  validate,
+  getFilteredConfig
+};
---
+++ DocService/sources/routes/config/router.js
@@ -0,0 +1,143 @@
+/*
+ * (c) Copyright Ascensio System SIA 2010-2024
+ *
+ * This program is a free software product. You can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License (AGPL)
+ * version 3 as published by the Free Software Foundation. In accordance with
+ * Section 7(a) of the GNU AGPL its Section 15 shall be amended to the effect
+ * that Ascensio System SIA expressly excludes the warranty of non-infringement
+ * of any third-party rights.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For
+ * details, see the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * You can contact Ascensio System SIA at 20A-6 Ernesta Birznieka-Upish
+ * street, Riga, Latvia, EU, LV-1050.
+ *
+ * The  interactive user interfaces in modified source and object code versions
+ * of the Program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU AGPL version 3.
+ *
+ * Pursuant to Section 7(b) of the License you must retain the original Product
+ * logo when distributing the program. Pursuant to Section 7(e) we decline to
+ * grant you any rights under trademark law for use of our trademarks.
+ *
+ * All the Product's GUI elements, including illustrations and icon sets, as
+ * well as technical writing content are licensed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International. See the License
+ * terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+ *
+ */
+
+'use strict';
+const config = require('config');
+const express = require('express');
+const bodyParser = require('body-parser');
+const tenantManager = require('../../../../Common/sources/tenantManager');
+const operationContext = require('../../../../Common/sources/operationContext');
+const runtimeConfigManager = require('../../../../Common/sources/runtimeConfigManager');
+const utils = require('../../../../Common/sources/utils');
+const { getFilteredConfig, validate } = require('./config.service');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const router = express.Router();
+
+router.use(cookieParser());
+
+const rawFileParser = bodyParser.raw(
+    {inflate: true, limit: config.get('services.CoAuthoring.server.limits_tempfile_upload'), type: function() {return true;}});
+
+const validateJWT = async (req, res, next) => {
+    let ctx = new operationContext.Context();
+    try {
+        ctx.initFromRequest(req);
+        await ctx.initTenantCache();
+        const token = req.cookies.accessToken;
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized - No token provided' });
+        }
+        
+        const defaultTenantSecret = config.get('services.CoAuthoring.secret.browser.string');
+        const tenantBaseDir = config.get('tenants.baseDir');
+        const filenameSecret = config.get('tenants.filenameSecret');
+        
+        try {
+            const decoded = jwt.verify(token, defaultTenantSecret);
+            if (ctx.tenant !== decoded.tenant) {
+                return res.status(401).json({ error: 'Unauthorized - Invalid tenant' });
+            }
+            req.user = decoded;
+            req.ctx = ctx;
+            return next();
+        } catch (defaultError) {
+            const tenantList = fs.readdirSync(tenantBaseDir);
+            for (const tenant of tenantList) {
+                try {
+                    const tenantSecret = fs.readFileSync(path.join(tenantBaseDir, tenant, filenameSecret), 'utf8');
+                    const decoded = jwt.verify(token, tenantSecret);
+                    if (ctx.tenant !== decoded.tenant) {
+                        return res.status(401).json({ error: 'Unauthorized - Invalid tenant' });
+                    }
+                    req.user = decoded;
+                    req.ctx = ctx;
+                    return next();
+                } catch (tenantError) {
+                    continue;
+                }
+            }
+            return res.status(401).json({ error: 'Unauthorized - Invalid token' });
+        }
+    } catch (error) {
+        console.log('JWT validation error:', error);
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+};
+
+router.get('/', validateJWT, async (req, res) => {
+    let ctx = req.ctx;
+    try {
+        ctx.logger.debug('config get start');
+        const filteredConfig = getFilteredConfig(ctx);
+        res.setHeader('Content-Type', 'application/json');
+        res.json(filteredConfig);
+        ctx.logger.debug('Config get success');
+    } catch (error) {
+        ctx.logger.error('Config get error: %s', error.stack);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.patch('/', validateJWT, rawFileParser, async (req, res) => {
+    let ctx = req.ctx;
+    try {
+        const currentConfig = ctx.getFullCfg();
+        const updateData = JSON.parse(req.body);
+        
+        const validationResult = validate(updateData, ctx);
+        if (validationResult.error) {
+            ctx.logger.error('Config save error: %s', validationResult.error);
+            return res.status(400).json({
+                error: validationResult.error
+            });
+        }
+
+        const newConfig = utils.deepMergeObjects(currentConfig, validationResult.value);
+        
+        if (tenantManager.isMultitenantMode(ctx) && !tenantManager.isDefaultTenant(ctx)) {
+            await tenantManager.setTenantConfig(ctx, newConfig);
+        } else {
+            await runtimeConfigManager.saveConfig(ctx, newConfig);
+        }
+
+        res.sendStatus(200);
+    } catch (error) {
+        ctx.logger.error('Configuration save error: %s', error.stack);
+        res.status(500).json({ error: 'Internal server error', details: error.message });
+    }
+});
+
+module.exports = router;
